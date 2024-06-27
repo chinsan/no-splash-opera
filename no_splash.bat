1c2d3881a56d59c39867f5af45dd98a97386e5eb
chinsan <chinsan@FreeBSD.org>
2024-06-28 00:19:20 +0800

    Update no_splash.bat

--- no_splash.bat
+++ no_splash.bat
@@ -3,15 +3,15 @@
 for %%I in (.) do set "directory=%%~dpI"
 
 for /d %%a in ("%directory%\*") do (
-    if exist "%%a\opera_gx_splash.exe" (
-        del "%%a\opera_gx_splash.exe"
-        @REM set "result=%%a\opera_gx_splash.exe"
+    if exist "%%a\opera_splash.exe" (
+        del "%%a\opera_splash.exe"
+        @REM set "result=%%a\opera_splash.exe"
     )
 )
 
 @REM echo %TIME% - %result% >> %~dp0\task_monitoring.log"
 
-@rem launch opera gx
+@rem launch opera
 "%directory%\launcher.exe"
 
 exit /b 0
